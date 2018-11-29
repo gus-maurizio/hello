efdd3f38512389a7fdc41166b783e09cd544617d
gustavo maurizio <gustavo,maurizio@gmail.com>
2018-11-29 07:24:14 -0500

    Thu Nov 29 07:24:14 EST 2018

--- hello.go
+++ hello.go
@@ -1,7 +1,52 @@
 package main
-  
-import "fmt"
+
+import (
+	log "github.com/sirupsen/logrus"
+	"os"
+)
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	//log.SetFormatter(&log.JSONFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.WarnLevel)
+        log.SetFormatter(&log.TextFormatter{
+                DisableColors: false,
+                FullTimestamp: true,
+                })
+        // This can be removed if CPU overhead is too high
+        //log.SetReportCaller(true)
+
+}
 
 func main() {
-        fmt.Println("Hello, world.")
+	log.WithFields(log.Fields{
+		"animal": "walrus",
+		"size":   10,
+	}).Info("A group of walrus emerges from the ocean")
+
+	log.WithFields(log.Fields{
+		"omg":    true,
+		"number": 122,
+	}).Warn("The group's number increased tremendously!")
+
+	log.WithFields(log.Fields{
+		"omg":    true,
+		"number": 100,
+	}).Fatal("The ice breaks!")
+
+	// A common pattern is to re-use fields between logging statements by re-using
+	// the logrus.Entry returned from WithFields()
+	contextLogger := log.WithFields(log.Fields{
+		"common": "this is a common field",
+		"other":  "I also should be logged always",
+	})
+
+	contextLogger.Info("I'll be logged with common and other field")
+	contextLogger.Info("Me too")
 }
