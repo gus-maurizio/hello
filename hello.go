7fe86b63ec9b8373f080231680ebdbfd93b2e904
Gustavo Maurizio <42898997+gus-maurizio@users.noreply.github.com>
2018-11-29 10:23:37 -0500

    Update hello.go

--- hello.go
+++ hello.go
@@ -16,6 +16,7 @@ func init() {
         log.SetLevel(log.DebugLevel)
         log.SetFormatter(&log.JSONFormatter{
                 //TimestampFormat: "RFC3339",
+                DisableTimestamp: true,
                 PrettyPrint: true,
                 })
         // This can be removed if CPU overhead is too high
@@ -27,11 +28,13 @@ func main() {
         log.WithFields(log.Fields{
                 "animal": "walrus",
                 "size":   10,
+                "timestamp": float64(time.Now().UnixNano())/1e9,
         }).Info("A group of walrus emerges from the ocean")
 
         log.WithFields(log.Fields{
                 "omg":    true,
                 "number": 122,
+                "timestamp": float64(time.Now().UnixNano())/1e9,
         }).Warn("The group's number increased tremendously!")
 
         // A common pattern is to re-use fields between logging statements by re-using
@@ -39,9 +42,10 @@ func main() {
         contextLogger := log.WithFields(log.Fields{
                 "common": "this is a common field",
                 "other":  "I also should be logged always",
+                "timestamp": float64(time.Now().UnixNano())/1e9,
         })
 
         contextLogger.Info("I'll be logged with common and other field")
         contextLogger.Info("Me too")
-        contextLogger.WithFields(log.Fields{"added": "new field", "timestamp": float64(time.Now().UnixNano())/1e9}).Info("Now Me too")
+        contextLogger.WithFields(log.Fields{"added": []string{"new field","extra"}, "timestamp": float64(time.Now().UnixNano())/1e9}).Info("Now Me too")
 }
