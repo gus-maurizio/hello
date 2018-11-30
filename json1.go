2ebfea4c343d647ae81c61fda9cc64b421b410e1
gustavo maurizio <gustavo,maurizio@gmail.com>
2018-11-30 05:20:21 -0500

    Fri Nov 30 05:20:21 EST 2018

---
+++ json1.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var jsonBlob = []byte(`[
+	{"Name": "Platypus", "Order": "Monotremata"},
+	{"Name": "Quoll",    "Order": "Dasyuromorphia"}
+]`)
+	type Animal struct {
+		Name  string
+		Order string
+	}
+	os.Stdout.Write(jsonBlob)
+	fmt.Println()
+	var animals interface{}
+	err := json.Unmarshal(jsonBlob, &animals)
+	if err != nil {
+		fmt.Println("unmarshal error:", err)
+	}
+	fmt.Printf("%+v \n", animals)
+        b, merr := json.Marshal(animals)
+        if merr != nil {
+                fmt.Println("marshal error:", merr)
+        }
+        os.Stdout.Write(b)
+	fmt.Println()
+}
