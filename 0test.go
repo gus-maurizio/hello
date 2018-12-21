804e44c540a565951edade22ea8d29da5106365f
gustavo maurizio <gustavo,maurizio@gmail.com>
2018-12-20 21:16:32 -0500

    Thu Dec 20 21:16:32 EST 2018

--- 0test.go
+++ 0test.go
@@ -7,16 +7,16 @@ import (
 
 func main() {
 	var qi duplexqueue.Duplexqueue
-	qi.Init(11,"----")
+	qi.Init(10,0)
         qi.Do( func(m interface{}) { fmt.Printf("%v, ",m) })
 	fmt.Printf("%v\n",qi)
 	fmt.Printf("\n") 
 	for i := 0; i < 25; i++ {
-		old := qi.PushPop(fmt.Sprintf("<%d, %v>",i,10+i))
+		old := qi.PushPop(10+i)
 		fmt.Printf("iter %3d old: %3v --> ",i,old) 
-        	qi.Do( func(m interface{}) { fmt.Printf("%v, ",m) })
-		fmt.Printf("\n") 
-		// fmt.Printf("%v\n",qi)
+        	qi.Do( func(m interface{}) { fmt.Printf("%2v, ",m) })
+		//fmt.Printf("\n") 
+		fmt.Printf(" 1: %3v 5: %3v 10: %3v buff:  %v\n",qi.Index(1-1), qi.Index(5-1), qi.Index(10-1), qi)
 	}
 
         fmt.Printf("-- DoFor ->")
@@ -27,4 +27,7 @@ func main() {
 
 	fmt.Printf("%v\n",qi)
 	fmt.Printf("%v\n",qi.Slice())
+	fmt.Printf("%v\n",qi.Index(3))
+	fmt.Printf("%v\n",qi.Index(-2))
+	fmt.Printf("%v\n",qi.Index(13))
 }
